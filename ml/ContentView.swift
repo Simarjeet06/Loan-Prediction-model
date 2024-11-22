248b5c3cddd2c580ee59a04fb4f4811a9a162992
Simarjeet Kaur <simarjeetkaur6603@gmail.com>
2024-11-23 01:34:03 +0530

    Initial Commit

---
+++ ml/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ml
+//
+//  Created by Simarjeet Kaur on 23/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ml/mlApp.swift
@@ -0,0 +1,17 @@
+//
+//  mlApp.swift
+//  ml
+//
+//  Created by Simarjeet Kaur on 23/11/24.
+//
+
+import SwiftUI
+
+@main
+struct mlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
